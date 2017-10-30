b64e020aca4c5adcd31cbee53e3316ac86122f6c
Ali G <ali.glenesk@gmail.com>
2017-10-29 18:56:30 -0700

    Home created with tweets rendered

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import json
 import oauth2 as oauth
 from secret import keys 
-from flask import (Flask, jsonify)
+from flask import (Flask, jsonify, render_template)
+import pprint #    pp = pprint.PrettyPrinter(indent=4) pp.pprint(stufftoprint)
 
 
 app = Flask(__name__)
@@ -15,11 +16,35 @@ def authorize():
     access_token = oauth.Token(key=keys["access_token"], secret=keys["access_secret"])
     client = oauth.Client(consumer, access_token)
 
-    test_url = "https://api.twitter.com/1.1/search/tweets.json?q=codenewbie&result_type=recent"
+    test_url = "https://api.twitter.com/1.1/search/tweets.json?q=%23codenewbie&result_type=mixed&count=100&include_entities=false"
     response, data = client.request(test_url)
 
-    return  json.loads(data)
+    return json.loads(data)
+
+
+@app.route("/")
+def homepage():
+    """Display tweets"""
+    
+
+    tweet = None
+    time_created = None
+    retweets = None
+    results = authorize()["statuses"]
+    output = []
+
+    for result in results:
+        tweet = result['text']
+        time_created =  result['created_at']
+        if 'retweeted_status' in result:
+            retweets = result['retweeted_status']['retweet_count']
+        output.append((tweet, time_created, retweets))
+
+    return render_template("home.html", output=output)
+
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    app.run(port=5000)
