c9f4a160ff5ac021d96dddbd3691ba787d9033c9
Heather Mahan <heatheramahan@gmail.com>
2018-05-18 15:54:54 -0700

    Small fixes related to secrets.
      * Improve documentation about secrets.
      * Add import and return statement to secret keys template.

--- secretkeys_template.py
+++ secretkeys_template.py
@@ -1,10 +1,14 @@
-### This script is not meant to be executed its merley a template that
-### Should be filled out and saved inside of a directory called secret
-### as keys.py
+### This script is not meant to be executed. It is merely a template that
+### should be filled out and saved keys.py inside of a directory called secret.
+### Be sure to include an empty __init__.py file in the secret directory so that
+### the directory can be imported as a module.
 
-## To create the Flask_Key use python import os os.urandom(45)
-## To obtain Keys (consumer_secret, access_secret, access_token, consumer_key)
-## Go to https://dev.twitter.com/apps/new and grab some keys
+## To create the Flask_Key, use os.urandom(45) from the os python standard
+## library.
+## To obtain Twitter credentials (consumer_secret, access_secret, access_token,
+## consumer_key), go to https://dev.twitter.com/apps/new.
+
+import os
 
 
 def key():
@@ -17,4 +21,4 @@ def key():
         'consumer_key': "REPLACE ME"
     }
 
-
+    return keys
